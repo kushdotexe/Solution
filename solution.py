b050d526e944883a5e0634c35d2a07f7323ecc6b
kushdotexe <kushmittal360@gmail.com>
2025-02-19 12:47:51 +0530

    Updated_solution.py
    This is the updated solution. I've added the validation for the names and corrected the ranking error. Now, two or more students can have same rank if they have same total marks.

--- solution.py
+++ solution.py
@@ -20,6 +20,24 @@ def get_valid_input(prompt, min_val, max_val):
         result = validate_marks(value, min_val, max_val)
         if result is not None:
             return result
+        
+def get_valid_input1(prompt, min_val, max_val):
+
+    while True:
+        value = input(prompt)
+        result2= validate_name(value, min_val, max_val)
+       
+        if result2 is not None:
+            return result2
+       
+                
+def validate_name(name, min_val, max_val):
+ 
+        if min_val<=len(name)<=max_val:
+            return name
+        else:
+            print("Error In Name, Please enter in specified syntax")
+            return None
 
 def main():
 
@@ -43,13 +61,14 @@ def main():
 
     for i in range(n):
         print(f"\nEntry {i+1}")
-        student_name = input("Enter Student Name (Max 30 chars): ")[:30]
-        college_name = input("Enter College Name (Max 50 chars): ")[:50]
+        student_name = get_valid_input1("Enter Student Name (Max 30 chars): ", 3, 30)
+        college_name = get_valid_input1("Enter College Name (Max 50 chars): ", 5, 50)
         
         r1 = get_valid_input("Enter Round 1 Marks (0-10): ", 0, 10)
         r2 = get_valid_input("Enter Round 2 Marks (0-10): ", 0, 10)
         r3 = get_valid_input("Enter Round 3 Marks (0-10): ", 0, 10)
         tech = get_valid_input("Enter Technical Round Marks (0-20): ", 0, 20)
+
 
         cursor.execute(
             '''INSERT INTO CANDIDATES1 
@@ -61,7 +80,13 @@ def main():
 
     cursor.execute("SET @rank := 0;")
     cursor.execute(
-        "UPDATE CANDIDATES1 SET RankOfStudents = (@rank := @rank + 1) ORDER BY TotalMarks DESC"
+        '''UPDATE CANDIDATES1 c
+        JOIN (
+            SELECT id, DENSE_RANK() OVER (ORDER BY TotalMarks DESC) AS new_rank
+            FROM CANDIDATES1
+        ) ranked
+        ON c.id = ranked.id
+        SET c.RankOfStudents = ranked.new_rank;'''
     )
     conn.commit()
 
